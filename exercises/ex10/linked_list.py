11add7db152ce1a2e7b788444bc45c5ff7fefbc4
Alana Schmidlin <alanaq@ad.unc.edu>
2024-04-28 13:10:27 -0400

    completed ex10 with 99.2

--- exercises/ex10/linked_list.py
+++ exercises/ex10/linked_list.py
@@ -1,4 +1,4 @@
-"""Utility functions for working with Linked Lists."""
+"""Methods and functions for working with Linked Lists, Node, and Recursion."""
 
 from __future__ import annotations
  
@@ -39,3 +39,47 @@ class Node:
             while current.next is not None:
                 current = current.next
             return current.data
+
+
+def value_at(head: Node | None, index: int) -> int:
+    """Return data of Node stores at given index."""
+    if head is None:  # edge case when list is empty
+        raise IndexError("Index is out of bounds on the list.")  
+    if index == 0:  # base case at index 0: return data of current Node
+        return head.data
+    return value_at(head.next, index - 1)  # modify head by moving to next and index by adding 1 to get closer to base
+
+
+def max(head: Node | None) -> int:
+    """Return maximum data in linked list."""
+    if head is None:
+        raise ValueError("Cannot call max with None")
+    max_value: int = head.data  # store max value, starting with base
+    current: Node = head  # index to move through linked list
+    while current is not None:
+        if current.data > max_value:  # compare stored max to current
+            max_value = current.data
+        current = current.next  # move to next Node
+    return max_value
+
+
+def linkify(items: list[int]) -> Node | None:
+    """Return linked list of Nodes with same vals in same order as input list."""
+    if not items:  # list is empty: base case
+        return None
+    head = Node(items[0], None)  # head node is first element in list
+    current = head
+    for item in items[1:]:  # python slice notation instead of len(items)
+        current.next = Node(item, None)
+        current = current.next  # link nodes
+    return head
+
+
+def scale(head: Node | None, factor: int) -> Node | None:
+    """Makes new linked list of Nodes with each value in og multipled by factor."""
+    if head is None:  # base case for empty list
+        return None
+    scaled_data = head.data * factor  # new node with data multiplied by factor
+    scaled_next = scale(head.next, factor)
+    scaled_node = Node(scaled_data, scaled_next)
+    return scaled_node
